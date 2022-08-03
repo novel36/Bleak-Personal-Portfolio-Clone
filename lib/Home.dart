8020ef3cd07b00a4ad3cdc15964b5ae26bfdcc86
novelwolde36 <novelwolde36@gmail.com>
2022-08-03 04:34:43 -0700

    i used Stickheader to stick the nav bar while scroll

--- lib/Home.dart
+++ lib/Home.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterresponsivenavigation/Sections/aboutMe.dart';
 import 'package:flutterresponsivenavigation/Sections/profileIntro.dart';
 import 'package:flutterresponsivenavigation/Sections/resume.dart';
-import 'package:sticky_web_scroll/sticky_web_scroll.dart';
+import 'package:sticky_headers/sticky_headers/widget.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -22,128 +22,118 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF212121),
-      body: StickyWebScroll(
-        stickyAppBar: Container(
-          color: Colors.blue,
-          width: MediaQuery.of(context).size.width,
-          child: Center(
-            child: Text('Stick Web Scroll - by Flutter Devs Core',
-                style: Theme.of(context).textTheme.headline4),
-          ),
-        ),
+        backgroundColor: Color(0xFF212121),
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.only(
                 left: 112, right: 112, top: 40, bottom: 64),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Color(0xff262626),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: TextButton(
-                          style:
-                              TextButton.styleFrom(padding: EdgeInsets.all(8)),
-                          onPressed: () {
-                            print("object");
-                          },
-                          child: Text(
-                            "BLEAK",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontSize: 25),
-                          ),
+            child: StickyHeader(
+              header: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Color(0xff262626),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: TextButton(
+                        style: TextButton.styleFrom(padding: EdgeInsets.all(8)),
+                        onPressed: () {
+                          print("object");
+                        },
+                        child: Text(
+                          "BLEAK",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 25),
                         ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                  padding: EdgeInsets.all(16)),
-                              onPressed: () {},
-                              child: Text(
-                                "Home",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                  padding: EdgeInsets.all(16)),
-                              onPressed: () {},
-                              child: Text(
-                                "About",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                  padding: EdgeInsets.all(16)),
-                              onPressed: () {},
-                              child: Text(
-                                "Contact Us",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                  padding: EdgeInsets.all(16)),
-                              onPressed: () {},
-                              child: Text(
-                                "Services",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                  padding: EdgeInsets.all(16)),
-                              onPressed: () {},
-                              child: Text(
-                                "Portfolio",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          SizedBox(
-                            width: 10,
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                            style: TextButton.styleFrom(
+                                padding: EdgeInsets.all(16)),
+                            onPressed: () {},
+                            child: Text(
+                              "Home",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                                padding: EdgeInsets.all(16)),
+                            onPressed: () {},
+                            child: Text(
+                              "About",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                                padding: EdgeInsets.all(16)),
+                            onPressed: () {},
+                            child: Text(
+                              "Contact Us",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                                padding: EdgeInsets.all(16)),
+                            onPressed: () {},
+                            child: Text(
+                              "Services",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                                padding: EdgeInsets.all(16)),
+                            onPressed: () {},
+                            child: Text(
+                              "Portfolio",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 64,
                   ),
-                ),
-                const SizedBox(
-                  height: 64,
-                ),
-                Profile(linearGradient: linearGradient),
-                SizedBox(
-                  height: 64,
-                ),
-                AboutMe(),
-                SizedBox(
-                  height: 64,
-                ),
-                MyResume()
-              ],
+                  Profile(linearGradient: linearGradient),
+                  SizedBox(
+                    height: 64,
+                  ),
+                  AboutMe(),
+                  SizedBox(
+                    height: 64,
+                  ),
+                  MyResume()
+                ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
